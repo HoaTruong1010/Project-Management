025ae1a8380c0f6136bb4e4477a11efbe7c89f48
Hoa Truong <2051050145hoa@ou.edu.vn>
2021-12-28 11:08:09 +0700

    first commit

---
+++ Resource Code/src/main/java/Project.java
@@ -0,0 +1,74 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class Project {
+    private static final SimpleDateFormat F = new SimpleDateFormat("dd/MM/yyyy");
+    private static int count = 0;
+    private String id, name;
+    private GregorianCalendar startDate, endDate;
+    private double investment;
+
+    {
+        this.id = String.format("%03d", ++count);
+        startDate = new GregorianCalendar();
+        endDate = new GregorianCalendar();
+    }
+
+    public Project(String n, String start, String end, double inv) throws ParseException {
+        this.name = n;
+        this.startDate.setTime(F.parse(start));
+        this.endDate.setTime(F.parse(end));
+        this.investment = inv;
+    }
+
+    public void show() {
+        System.out.printf("- Ma du an: %s\n- Ten du an: %s\n- Thoi gian: %s - %s\n" +
+                "- Phi dau tu: %.1f\n", this.id, this.name, F.format(this.startDate.getTime()),
+                F.format(this.endDate.getTime()), this.investment);
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GregorianCalendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(GregorianCalendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public GregorianCalendar getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(GregorianCalendar endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getInvestment() {
+        return investment;
+    }
+
+    public void setInvestment(double investment) {
+        this.investment = investment;
+    }
+
+    public static void main(String[] args) throws ParseException {
+        Project p1 = new Project("Nguyen Van A", "19/07/2002", "19/07/2002", 1000.0);
+    }
+}
