d78dd16869db088094a4c7cbbf5d09d0f0208393
Saki2502 <tuyetnhi1610@gmail.com>
2022-01-04 20:54:57 +0700

    update getGrant Designer 4-1-22

--- Project_Management/src/main/java/Designer.java
+++ Project_Management/src/main/java/Designer.java
@@ -3,7 +3,6 @@ import java.util.Date;
 
 public class Designer extends Staff {
     private int numProject;
-    private static final int MINPROJECT = 2;
     private static final int MAXPROJECT = 3;
 
     public Designer() {
@@ -35,8 +34,8 @@ public class Designer extends Staff {
         do {
             this.numProject = scanner.nextInt();
             if (this.numProject < 0 || this.numProject > 3) System.out.println("Vui lòng nhập số nguyên dương!");
-        } while (this.numProject < 0 || this.numProject > 3);
-        return (this.numProject == MINPROJECT || this.numProject == MAXPROJECT) ? 0 : this.numProject * 200000;
+        } while (this.numProject <= 0 || this.numProject > MAXPROJECT);
+        return (this.numProject - 1) * 200000;
     }
 
     public int getNumProject() {
